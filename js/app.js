9f10aef40257ef686d1c7a27854607f3532d5878
kimtaewan <leexodhks12@naver.com>
2021-05-26 17:34:54 +0900

    커밋!

--- js/app.js
+++ js/app.js
@@ -12,36 +12,38 @@ let test = document.querySelectorAll('.header__nav li');
 
 test.forEach( v => {
     v.addEventListener('click', ()=>{
-        v.classList.toggle('on')
+        v.classList.add('on')
         siblings(v, 'on')
     })
 } )
 
-// function siblings (el, className) {
-//     let cs = el.nextElementSibling;
-
-//     while(cs){ // 만약 다음 형제요소가 있으면
-//         cs.classList.remove(className);
-//         cs = cs.nextElementSibling;
-//     }
-//     // 다음 형제요소가 없다면
-//     cs = el.previousElementSibling;
-//     while(cs){
-//         cs.classList.remove(className);
-//         cs = cs.previousElementSibling;
-//     }
-
-// }
-
-
-function siblings(el, cn) {
+function siblings (el, className) {
     let cs = el.nextElementSibling;
-    cs.classList.remove(cn);
-    while (cs) {
-      if (cs) {
+
+    while(cs){ // 만약 다음 형제요소가 있으면
+        cs.classList.remove(className);
         cs = cs.nextElementSibling;
-      } else {
-        cs = cs.previousElementSibling;
-      }
     }
-  }
+    // 다음 형제요소가 없다면
+    cs = el.previousElementSibling;
+    while(cs){
+        cs.classList.remove(className);
+        cs = cs.previousElementSibling;
+    }
+
+}
+
+
+function closeParentTarget(parentClassName){
+  console.log(parentClassName);
+  this.closest(parentClassName).closest('li').classList.remove('on') 
+  
+
+}
+
+function open(el, display){
+  el.style.display = display;
+}
+function toggleClass(className){
+  this.addClass.toggle(className)
+}

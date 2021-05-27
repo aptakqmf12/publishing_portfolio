c09e0967056e93114ce2adb361f21bee2464176e
kimtaewan <leexodhks12@naver.com>
2021-05-27 17:41:41 +0900

    셀렉팅 스크립트추가 / 반응형 믹스인추가

--- js/app.js
+++ js/app.js
@@ -1,49 +1,87 @@
-// loading박스
-let isAnimation = true;
-let loadingBox = document.querySelector('.loading');
-let loading = document.querySelector('.loading .left');
-
-loading.addEventListener('animationend', ()=> {
-    loadingBox.classList.add('hide');
-})
-
-//nav 클릭시
-let test = document.querySelectorAll('.header__nav li');
-
-test.forEach( v => {
-    v.addEventListener('click', ()=>{
-        v.classList.add('on')
-        siblings(v, 'on')
-    })
-} )
-
-function siblings (el, className) {
-    let cs = el.nextElementSibling;
-
-    while(cs){ // 만약 다음 형제요소가 있으면
-        cs.classList.remove(className);
-        cs = cs.nextElementSibling;
+const qs = (el, option) => {
+  if (document.querySelectorAll(el).length > 0) {
+    if (option == "array") {
+      return document.querySelectorAll(el);
     }
-    // 다음 형제요소가 없다면
-    cs = el.previousElementSibling;
-    while(cs){
-        cs.classList.remove(className);
-        cs = cs.previousElementSibling;
+    if (document.querySelectorAll(el).length == 1) {
+      return document.querySelector(el);
+    } else {
+      return document.querySelectorAll(el);
     }
+  }
+};
+const findSibilings = () => {
+  if (option == "other") {
+    var res = [...currentEl.children];
+    res.splice([...currentEl.children].indexOf(el), 1);
+    return res;
+  }
+  return currentEl.children;
+};
+const closest = (startEl, selector) => {
+  let currentEl = startEl;
 
+  while (!currentEl.classList.contains(selector)) {
+    if (currentEl.tagName === "HTML") {
+      return currentEl;
+    }
+    currentEl = currentEl.parentElement;
+  }
+
+  return currentEl;
 }
+function siblings (el, className) {
+  let cs = el.nextElementSibling;
 
-
+  while(cs){ // 만약 다음 형제요소가 있으면
+      cs.classList.remove(className);
+      cs = cs.nextElementSibling;
+  }
+  // 다음 형제요소가 없다면
+  cs = el.previousElementSibling;
+  while(cs){
+      cs.classList.remove(className);
+      cs = cs.previousElementSibling;
+  }
+}
 function closeParentTarget(parentClassName){
   console.log(parentClassName);
   this.closest(parentClassName).closest('li').classList.remove('on') 
-  
-
 }
 
-function open(el, display){
-  el.style.display = display;
-}
-function toggleClass(className){
-  this.addClass.toggle(className)
-}
+
+
+
+
+  // loading박스
+  let isAnimation = true;
+  let loadingBox = qs('.loading');
+  let loading = qs('.loading .left');
+
+  loading.addEventListener('animationend', ()=> {
+      loadingBox.classList.add('hide');
+  })
+
+  //nav 클릭시
+  let NavList = qs('.header__nav > li', 'array');
+
+  NavList.forEach( v => {
+      v.addEventListener('click', ()=>{
+        v.classList.add('on')
+        siblings(v, 'on')
+
+      })
+  } )
+
+  let NavCloseBtns = qs('.header__fullSub .close-btn', 'array');
+
+  NavCloseBtns.forEach( v => {
+    v.addEventListener('click', e => {
+      e.preventDefault
+      let target = e.target;
+      target.closest('li').classList.remove('on');
+    })
+  })
+
+
+
